ad24b9cb4cd932deccca0704c97b44c1d043ba4c
Kapish Sharma <kapish@mindrops.com>
2021-12-13 17:38:29 +0530

    init

---
+++ dist/js/script.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function () {
+   $(".flip-btn").click(function () {
+      $(".circle-wrap").toggle();
+      $(".flip-progress").toggle();
+   });
+});
+
+$(document).ready(function () {
+   $(".accordion-button").click(function () {
+      $(".plus-icon").toggle();
+      $(".minus-icon").toggle();
+   });
+});
